6d85e565af8a683bddd8228db4ce6c8781c97b33
zerojin <qser155@naver.com>
2023-08-02 14:05:05 +0900

    ###

--- app/hooks/useConversation.ts
+++ app/hooks/useConversation.ts
@@ -5,11 +5,11 @@ const useConversation = () => {
     const params = useParams();
 
     const conversationId = useMemo(() => {
-        if (!params.conversationId) {
+        if (!params?.conversationId) {
             return '';
         }
         return params.conversationId as string;
-    }, [params.conversationId]);
+    }, [params?.conversationId]);
 
     const isOpen = useMemo(() => !!conversationId, [conversationId]);
 
